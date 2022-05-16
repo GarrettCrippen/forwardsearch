0f88ce2ff832883ab1575e52adc07a0dfbb02bab
GarrettCrippen <garrettcrippen@gmail.com>
2022-05-16 13:13:18 -0700

    rename and create driver

---
+++ driver.py
@@ -0,0 +1,20 @@
+import search
+search_alg=[search.forward_selection,search.backward_elimination]
+while(True):
+    print('Enter total number of features: ',end='')
+    num = input()
+    try:
+        val = int(num)
+    except:
+        print('Error: not a number')
+        break
+    print('Please enter the search algorithm you would like to run.')
+
+    for i,alg in enumerate(search_alg):
+        print(f'{i+1}) {alg.__name__}')
+
+    choice = input()
+    if choice == '1' or choice =='2':
+        search_alg[int(choice)-1](int(num))
+    else:
+        break
