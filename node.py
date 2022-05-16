172233dfbaecf630f0cce1ac9bf079e3c5ce16c3
GarrettCrippen <garrettcrippen@gmail.com>
2022-05-16 11:54:55 -0700

    use iterations instead of recusion

--- node.py
+++ node.py
@@ -1,38 +1,37 @@
 #reference https://stackoverflow.com/questions/4010322/sort-a-list-of-class-instances-python
+#reference project2-starter code
 import random
-import operator
-random.seed('project2')
-class node:
-    def __init__(self,num_features,cost=0,total_nodes=1,state=(),parent=None,depth=0,visited_set=set(),frontier = []):
-        self.state=state
-        self.parent=parent
-        self.children=[]
-        self.depth=depth
-        self.cost=cost
-        node.total_nodes=total_nodes
-        node.visited_set=visited_set  
-        node.frontier = frontier
-        self.num_features=num_features
-    #select one feature at a time
-    def forward_selection(self):
-        if self.num_features > 0:
-            child=None
-            #add node to visited set
-            if self.state not in self.visited_set:
-                self.visited_set.add(self.state)
-            for i in range(1,self.num_features):
-                new_state = frozenset(sorted((*self.state,i)))
-                if new_state not in self.visited_set:
-                    r=random.randrange(1,50)
-                    print(f'creating child:{new_state} from: {self.state} with accuracy: {r} at depth: {self.depth}')
-                    child = node(num_features=self.num_features-1,parent = self, depth = self.depth+1, cost = r,state = new_state)
-                    self.children.append(child)
-                    self.frontier.append(child)
-            sorted(self.frontier,key=operator.attrgetter('cost'))
-            while len(self.frontier):
-                c=self.frontier.pop()
-                c.forward_selection()
+
+#select one feature at a time
+def forward_selection(num_features):
+    random.seed()
+    best_set=(0,0)
+    #this is the depth of the tree
+    features = ()
+    for i in range(1,num_features+1):
+        best_accuracy=0.0
+        a=0
+        #these are the features to add at each depth
+        for k in range(1,num_features+1):
+            #check the intersection between feature and set of features
+            if not k in set(features):
+                #possible feature selection
+                a=round(random.uniform(0,1),3)
+                #print(f'feature:{k} accuracy:{a}')
+                #continue searching the best feature selection
+                if a > best_accuracy:
+                    best_accuracy = a
+                    feature_to_add = k  
+        #print(f'adding {feature_to_add}')
+        features=sorted((*features,feature_to_add))
+        #fin
+        if best_accuracy > best_set[1]:
+            best_set=(features,best_accuracy)
+        print(f"Using feature(s): {features}      accuracy is {format(best_accuracy,'.3f')}%")
+
+    print(f"\nfeature(s): {best_set[0]} was the best, accuracy is {format(best_set[1],'.3f')}")
 
 
-n = node(5)
-n.forward_selection()
+
+
+forward_selection(5)
