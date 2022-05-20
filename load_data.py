03f76305ae0ea1d654232eb2167c3b7376b14729
GarrettCrippen <garrettcrippen@gmail.com>
2022-05-20 00:41:07 -0700

    need to make it so it can change feature select

--- load_data.py
+++ load_data.py
@@ -1,35 +1,41 @@
+#https://stackoverflow.com/questions/13028120/split-string-by-arbitrary-number-of-white-spaces
+#https://stackoverflow.com/questions/10695139/sort-a-list-of-tuples-by-2nd-item-integer-value
+
 import numpy as np
 import re
 from operator import itemgetter
 
+#find the nearest neighbors validator (for all features)
+def NNValidator(file_name):
+    distances = []
+    #load data from file
+    data= np.loadtxt(file_name,delimiter='\t',dtype=str)
+    total_datapoints = len(data)
+    correct = 0
+
+    #Leave each data point out
+    for k in range(total_datapoints):
+        row = re.split(r'\s+',data[k][2:])
+        test_class = row[0]
+        test_features = row[1:]
+
+        for i in range(total_datapoints):
+            neighbor = re.split(r'\s+',data[i][2:])
+            neighbor_class = neighbor[0]
+            neighbor_features = neighbor[1:]
+
+            #calculate the distance between the points
+            if i != k:
+                dist = np.linalg.norm(np.array(test_features,dtype=float)-np.array(neighbor_features,dtype=float))
+                distances.append((dist, neighbor_class))
+        distances = sorted(distances,key = itemgetter(0))
+        
+        if distances[0][1] == test_class:
+            correct+=1
+            #print('correct for: '+str(k))
+
+    #sort the distances from the neighbors
+    print(f'Accuracy is: {correct/(total_datapoints-1)}')        
+
 file_name = 'small-test-dataset.txt'
-distances = []
-
-#load data from file
-data= np.loadtxt(file_name,delimiter='\t',dtype=str)
-
-for k in range(1):
-    row = re.split(r'\s+',data[k][2:])
-    test_class = row[0]
-    test_features = row[1:]
-
-    for i in range(len(data)):
-        neighbor = re.split(r'\s+',data[i][2:])
-        neighbor_class = neighbor[0]
-        neighbor_features = neighbor[1:]
-
-        #calculate the distance between the points
-        if i != k:
-            dist = np.linalg.norm(np.array(test_features,dtype=float)-np.array(neighbor_features,dtype=float))
-            distances.append((dist, neighbor_class))
-
-distances = sorted(distances,key = itemgetter(0))
-print(distances)
-correct = 0
-for i in range(25):
-    if float(distances[i][1])>1:
-        correct+=1
-print(correct)        
-# row = data[1][2:].split('  ')
-# print(row)
-# print(row[1:])
+NNValidator(file_name)
